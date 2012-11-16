d90762893a604fc314cd1ba0377bde972aaed95a
Tim Woo <timwoo91@gmail.com>
2012-11-16 02:01:39 -0800

    all parsing done and output to jsp

--- build/itemResult.jsp
+++ build/itemResult.jsp
@@ -4,31 +4,27 @@
 </head>
 <body>
 	<form name="input" action="item" method="get">
-
 	<%
 	    String q = request.getParameter("itemID");
 		out.println("Looked up Item ID: <input type=\"text\" name=\"itemID\" value=\"" + q +"\">");
 	%>
-
 	<input type="submit" value="Submit"><br>
-   
-   <br><br>
+	</form>
 
-   <%
-        out.println(request.getAttribute("xml"));
-
-   %>
-   <br>
-   <br>
-   <p>Name: <%= request.getAttribute("name")%></p>
-  <p> Description: <%= request.getAttribute("Description")%></p>
-   <p>Seller: <%= request.getAttribute("SellerID")%></p>
-  <p>Seller Rating: <%= request.getAttribute("SellerRating")%></p>
-
-   <p>Started: <%= request.getAttribute("Started")%></p>
-   <p>Ends: <%= request.getAttribute("Ends")%></p>
-
-   </form>
+	<p><b>Name</b>: <%= request.getAttribute("name")%></p>
+	<p><b>Description</b>: <%= request.getAttribute("Description")%></p>
+	<p><b>Seller</b>: <%= request.getAttribute("SellerID")%></p>
+	<p><b>Seller Rating</b>: <%= request.getAttribute("SellerRating")%></p>
+	<p><b>Started</b>: <%= request.getAttribute("Started")%></p>
+	<p><b>Ends</b>: <%= request.getAttribute("Ends")%></p>
+	<p><b>Bids</b>: <%= request.getAttribute("Bids")%></p>
+	<p><b>Categories</b>: <%= request.getAttribute("Categories")%></p>
+	<p><b>Currently</b>: <%= request.getAttribute("Currently")%></p>
+	<p><b>FirstBid</b>: <%= request.getAttribute("First_Bid")%></p>
+	<p><b>Number of Bids</b>: <%= request.getAttribute("Number_of_Bids")%></p>
+	<p><b>Categories</b>: <%= request.getAttribute("Categories")%></p>
+	<p><b>Location</b>: <%= request.getAttribute("Location")%></p>
+	<p><b>Country</b>: <%= request.getAttribute("Country")%></p>
 
 </body>
 </html>
--- src/edu/ucla/cs/cs144/ItemServlet.java
+++ src/edu/ucla/cs/cs144/ItemServlet.java
@@ -118,132 +118,90 @@ public class ItemServlet extends HttpServlet implements Servlet {
 
   	    AuctionSearchClient sc = new AuctionSearchClient();
   	    String result = sc.getXMLDataForItemId(itemID);
-  	    request.setAttribute("xml", result);
+  	    // request.setAttribute("xml", result);
 
   			Document doc = null;
   			Element root = null;
   			DocumentBuilder builder = null;
   		   
-  		    if (result.isEmpty())
-  		    {
-  		    	request.setAttribute("total", 0);
-  		    }
-  	    	else 
-  	    	{
-  	    		request.setAttribute("total", 1);
-  	    		
-  	    		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-  	    		try {
-          			builder = factory.newDocumentBuilder();
-          	}
-          	catch (ParserConfigurationException e)
-          	{
-          		e.printStackTrace();
-      				System.exit(3);
-          	}
+		    if (result.isEmpty())
+		    {
+		    	request.setAttribute("total", 0);
+		    }
+	    	else 
+	    	{
+	    		request.setAttribute("total", 1);
+	    		
+	    		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+	    		try {
+        			builder = factory.newDocumentBuilder();
+        	}
+        	catch (ParserConfigurationException e)
+        	{
+        		e.printStackTrace();
+    				System.exit(3);
+        	}
 
-         		InputSource is = new InputSource(new StringReader(result));
+       		InputSource is = new InputSource(new StringReader(result));
 
-  				  try {
-              doc = builder.parse(is);
-    				}
-    				catch (IOException e) {
-    					e.printStackTrace();
-    					System.exit(3);
-    				}
-    				catch (SAXException e) {
-    					e.printStackTrace();
-    					System.exit(3);
-    				}
+				  try {
+            doc = builder.parse(is);
+  				}
+  				catch (IOException e) {
+  					e.printStackTrace();
+  					System.exit(3);
+  				}
+  				catch (SAXException e) {
+  					e.printStackTrace();
+  					System.exit(3);
+  				}
 
-    				String categories = "";
-  					root = doc.getDocumentElement();
-            request.setAttribute("name", getElementTextByTagNameNR(root,"Name"));
-            request.setAttribute("Currently", getElementTextByTagNameNR(root,"Currently"));
-            request.setAttribute("First_Bid", getElementTextByTagNameNR(root,"First_Bid"));
-            request.setAttribute("Number_of_Bids", getElementTextByTagNameNR(root,"Number_of_Bids"));
-            request.setAttribute("Location", getElementTextByTagNameNR(root,"Location"));
-            request.setAttribute("Country", getElementTextByTagNameNR(root,"Country"));
-            request.setAttribute("Ends", getElementTextByTagNameNR(root,"Ends"));
-            request.setAttribute("Started", getElementTextByTagNameNR(root,"Started"));
-            request.setAttribute("Description", getElementTextByTagNameNR(root,"Description"));
+					root = doc.getDocumentElement();
+          request.setAttribute("name", getElementTextByTagNameNR(root,"Name"));
+          request.setAttribute("Currently", getElementTextByTagNameNR(root,"Currently"));
+          request.setAttribute("First_Bid", getElementTextByTagNameNR(root,"First_Bid"));
+          request.setAttribute("Number_of_Bids", getElementTextByTagNameNR(root,"Number_of_Bids"));
+          request.setAttribute("Location", getElementTextByTagNameNR(root,"Location"));
+          request.setAttribute("Country", getElementTextByTagNameNR(root,"Country"));
+          request.setAttribute("Ends", getElementTextByTagNameNR(root,"Ends"));
+          request.setAttribute("Started", getElementTextByTagNameNR(root,"Started"));
+          request.setAttribute("Description", getElementTextByTagNameNR(root,"Description"));
 
-            Element sellerElement = getElementByTagNameNR(root,"Seller");
-            request.setAttribute("SellerID", getAttributeTextByTagNameNR(sellerElement,"UserID"));
-            request.setAttribute("SellerRating", getAttributeTextByTagNameNR(sellerElement,"Rating"));
+          Element sellerElement = getElementByTagNameNR(root,"Seller");
+          request.setAttribute("SellerID", getAttributeTextByTagNameNR(sellerElement,"UserID"));
+          request.setAttribute("SellerRating", getAttributeTextByTagNameNR(sellerElement,"Rating"));
 
-
-  					// Node child = root.getFirstChild();
-     		// 		while (child != null) 
-     		// 		{
-       //   				if (child instanceof Element && child.getNodeName().equals("Name"))
-       //   				{
-       //            String name = getElementTextByTagNameNR(root,"Name");
-       //   					request.setAttribute("name", name);
-       //   				}
-       //   				else if (child instanceof Element && child.getNodeName().equals("Category"))
-       //   				{
-       //   					if (!categories.equals(""))
-       //   					{
-       //   						categories += ", ";
-       //   					}
-       //   					categories += child.getNodeValue();
-       //   				}
-       //   				else if (child instanceof Element && child.getNodeName().equals("Currently"))
-       //   				{
-       //   					request.setAttribute("currently", child.getNodeValue());
-       //   				}
-       //   				else if (child instanceof Element && child.getNodeName().equals("First_Bid"))
-       //   				{
-       //   					request.setAttribute("first", child.getNodeValue());
-       //   				}
-       //   				else if (child instanceof Element && child.getNodeName().equals("Number_of_Bids"))
-       //   				{
-       //   					request.setAttribute("numBids", child.getNodeValue());
-       //   				}
-       //   				else if (child instanceof Element && child.getNodeName().equals("Bids"))
-       //   				{
-       //   					//request.setAttribute("numBids", child.getNodeValue());
-       //   				}
-       //   				else if (child instanceof Element && child.getNodeName().equals("Location"))
-       //   				{
-       //   					request.setAttribute("location", child.getNodeValue());
-       //   				}
-       //   				else if (child instanceof Element && child.getNodeName().equals("Country"))
-       //   				{
-       //   					request.setAttribute("country", child.getNodeValue());
-       //   				}
-       //   				else if (child instanceof Element && child.getNodeName().equals("Started"))
-       //   				{
-       //   					request.setAttribute("started", child.getNodeValue());
-       //   				}
-       //   				else if (child instanceof Element && child.getNodeName().equals("Ends"))
-       //   				{
-       //   					request.setAttribute("ends", child.getNodeValue());
-       //   				}
-       //   				else if (child instanceof Element && child.getNodeName().equals("Seller"))
-       //   				{
-       //   					//request.setAttribute("started", child.getNodeValue());
-       //   				}
-       //   				else if (child instanceof Element && child.getNodeName().equals("Description"))
-       //   				{
-       //   					request.setAttribute("description", child.getNodeValue());
-       //   				}
-
-	      //       	child = child.getNextSibling();
-      	// 		}
-          			//request.setAttribute("category", categories);
-  				//}
-
+          Element bidE = getElementByTagNameNR(root, "Bids");
+          Element[] bidArr = getElementsByTagNameNR(bidE, "Bid");
+          String bids = "";
+          for (int i=0;i<bidArr.length;i++) {
+            Element bidder = getElementByTagNameNR(bidArr[i], "Bidder");
+            String bidderID = getAttributeTextByTagNameNR(bidder, "UserID");
+            String bidderRating = getAttributeTextByTagNameNR(bidder, "Rating");
+            String location = getElementTextByTagNameNR(bidder, "Location");
+            String country = getElementTextByTagNameNR(bidder, "Country");
+            String time = getElementTextByTagNameNR(bidArr[i],"Time");
+            String amount = getElementTextByTagNameNR(bidArr[i],"Amount");
+            bids += "<br>BidderID: " + bidderID + "<br>BidderRating: " + bidderRating + "<br>BidderLocation: " +  location + "<br>BidderCountry: " + country + "<br>Bid Time: " + time + "<br>Bid Amount: " + amount + "<br><br>";
           }
-  		    request.getRequestDispatcher("/itemResult.jsp").forward(request, response);
+          request.setAttribute("Bids", bids);
 
-      	}
-      	else {
-  		    request.getRequestDispatcher("/getItem.html").forward(request, response);
-      	}
+          Element[] catArr = getElementsByTagNameNR(root, "Category");
+          String categories = "";
+          for (int i=0;i<catArr.length;i++) {
+            categories += getElementText(catArr[i]);
+            if (i!=0 && i!=catArr.length-1) {
+              categories += ", ";
+            }
+          }
+          request.setAttribute("Categories", categories);
+        }
+		    request.getRequestDispatcher("/itemResult.jsp").forward(request, response);
 
-        // your codes here
+      }
+      else {
+  	    request.getRequestDispatcher("/getItem.html").forward(request, response);
+    	}
     }
 }
 
